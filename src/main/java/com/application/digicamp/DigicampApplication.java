f29c05d3ecfec37e0f02bff3c9e5b25c21d98306
all-in-2 <nicu_fg@yahoo.com>
2022-09-03 13:09:18 +0300

    First commit
    Spring Initializr + Gradle Nature

---
+++ src/main/java/com/application/digicamp/DigicampApplication.java
@@ -0,0 +1,13 @@
+package com.application.digicamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigicampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/digicamp/DigicampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.digicamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigicampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
